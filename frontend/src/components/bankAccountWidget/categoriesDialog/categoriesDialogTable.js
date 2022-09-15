f88b8a1d75ad79ae69144c275c64e644b76a7f0d
JanKnapen <44979696+JanKnapen@users.noreply.github.com>
2022-09-15 11:20:08 +0200

    CategoryIcon in Dialog

--- frontend/src/components/bankAccountWidget/categoriesDialog/categoriesDialogTable.js
+++ frontend/src/components/bankAccountWidget/categoriesDialog/categoriesDialogTable.js
@@ -1,21 +1,35 @@
 import {Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
-import {useContext} from "react";
+import {createElement, useContext} from "react";
 import BankContext from "../../../contexts/BankContext";
+import UtilsContext from "../../../contexts/UtilsContext";
 
 function CategoriesDialogTable({ setOpenEditCategory, setCategoryEditId }) {
     const { categories } = useContext(BankContext);
+    const { MUIIcons } = useContext(UtilsContext);
+
+    const getIcon = (iconId) => {
+        const icon = MUIIcons.filter(MUIIcon => MUIIcon.id === iconId)[0].icon;
+        return (
+            createElement(icon, {key: icon}, null)
+        )
+    }
 
     return (
         <>
             <Table>
                 <TableHead>
                     <TableRow>
+                        <TableCell width={50} tooltip="The Icon">
+                            <b>Icon</b>
+                        </TableCell>
                         <TableCell width={300} tooltip="The Name">
                             <b>Name</b>
                         </TableCell>
-                        <TableCell tooltip="The Description">
+                        <TableCell width={400} tooltip="The Description">
                             <b>Description</b>
+                        </TableCell>
+                        <TableCell width={100} tooltip="Edit">
                         </TableCell>
                     </TableRow>
                 </TableHead>
@@ -27,9 +41,10 @@ function CategoriesDialogTable({ setOpenEditCategory, setCategoryEditId }) {
                             <TableRow
                                 key={category.id}
                             >
+                                <TableCell width={50}>{getIcon(category.icon)}</TableCell>
                                 <TableCell width={300}>{category.name}</TableCell>
                                 <TableCell width={400}>{category.description}</TableCell>
-                                <TableCell>
+                                <TableCell width={100}>
                                     <Button
                                         variant='contained'
                                         onClick={(event) => {
