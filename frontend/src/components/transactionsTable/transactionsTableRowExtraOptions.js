b1836ceef9160892f9474434874f45bfe170bb55
JanKnapen <44979696+JanKnapen@users.noreply.github.com>
2022-11-28 19:25:39 +0100

    Update paymentrequests array

--- frontend/src/components/transactionsTable/transactionsTableRowExtraOptions.js
+++ frontend/src/components/transactionsTable/transactionsTableRowExtraOptions.js
@@ -3,19 +3,19 @@ import {Button, Checkbox, TextField} from "@mui/material";
 import {useContext, useState} from "react";
 import BankContext from "../../contexts/BankContext";
 
-function TransactionsTableRowExtraOptions({ transaction, setTransactions }) {
+function TransactionsTableRowExtraOptions({transaction, setTransactions}) {
     const {
+        paymentRequests,
         setPaymentRequests,
     } = useContext(BankContext);
     const [openExtraOptions, setOpenExtraOptions] = useState(false);
     const [isPaymentRequest, setIsPaymentRequest] = useState(false);
     const [paymentRequestAmount, setPaymentRequestAmount] = useState(transaction.amount > 0 ? transaction.amount : null);
-    const [originalTransactionAmount, _] = useState(transaction.amount);
+    const [originalTransactionAmount, _] = useState(transaction.amount); // eslint-disable-line
 
     const updateTransactionAmount = (amount) => {
-        const transactionSerialNumber = transaction.serial_number;
         setTransactions(prevTransactions => prevTransactions.map(prevTransaction => {
-            if (prevTransaction.serial_number === transactionSerialNumber) {
+            if (prevTransaction.serial_number === transaction.serial_number) {
                 return {
                     ...prevTransaction,
                     amount: amount,
@@ -25,12 +25,50 @@ function TransactionsTableRowExtraOptions({ transaction, setTransactions }) {
         }))
     }
 
+    const paymentRequestExists = () => {
+        return paymentRequests.some(paymentRequest =>
+            paymentRequest.original_bank_transaction === transaction.serial_number
+        );
+    }
+
+    const createPaymentRequest = () => {
+        setPaymentRequests(prevPaymentRequests => [
+            ...prevPaymentRequests,
+            {
+                original_bank_transaction: transaction.serial_number,
+                amount: paymentRequestAmount,
+                bank_account: transaction.bank_account,
+            },
+        ])
+    }
+
+    const updatePaymentRequestAmount = () => {
+        if (!paymentRequestExists()) {
+            createPaymentRequest();
+        } else {
+            setPaymentRequests(prevPaymentRequests => prevPaymentRequests.map(prevPaymentRequest => {
+                if (prevPaymentRequest.original_bank_transaction === transaction.serial_number) {
+                    return {
+                        ...prevPaymentRequest,
+                        amount: paymentRequestAmount,
+                    }
+                }
+                return prevPaymentRequest;
+            }))
+        }
+    }
+
+    const removePaymentRequest = () => {
+        setPaymentRequests(prevPaymentRequests => prevPaymentRequests.filter(
+            paymentRequest => paymentRequest.original_bank_transaction !== transaction.serial_number
+        ));
+    }
+
     const toggleOpenExtraOptions = () => {
         setOpenExtraOptions(prevState => {
-            if (prevState === true) {
-                updateTransactionAmount(originalTransactionAmount);
-                setIsPaymentRequest(false);
-            }
+            updateTransactionAmount(originalTransactionAmount);
+            removePaymentRequest();
+            setIsPaymentRequest(false);
             return !prevState;
         });
     }
@@ -38,8 +76,10 @@ function TransactionsTableRowExtraOptions({ transaction, setTransactions }) {
     const togglePaymentRequestOption = () => {
         setIsPaymentRequest(prevState => {
             if (prevState === true) {
+                removePaymentRequest();
                 updateTransactionAmount(originalTransactionAmount);
             } else {
+                updatePaymentRequestAmount();
                 updateTransactionAmount(originalTransactionAmount - paymentRequestAmount);
             }
             return !prevState;
@@ -56,6 +96,7 @@ function TransactionsTableRowExtraOptions({ transaction, setTransactions }) {
         if (event.target.value < 0 || event.target.value > transaction.amount) {
             setPaymentRequestAmount(originalTransactionAmount);
         } else {
+            updatePaymentRequestAmount();
             updateTransactionAmount(originalTransactionAmount - paymentRequestAmount);
         }
     }
